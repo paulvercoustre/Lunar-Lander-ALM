32838a15515cfd71c0254eb51ada2978cec3d4cc
paulvercoustre <paulvercoustre@gmail.com>
2017-03-08 13:46:46 +0100

    Add Gym script

--- Lunar_Lander.py
+++ Lunar_Lander.py
@@ -1,7 +1,7 @@
 import gym
 env = gym.make('LunarLanderContinuous-v2')
 env.reset()
-for _ in range(100): # testing changes to number of epochs per episode
+for _ in range(100):  # testing changes to number of epochs per episode
     env.render()
     env.step(env.action_space.sample())  # take a random action
 
