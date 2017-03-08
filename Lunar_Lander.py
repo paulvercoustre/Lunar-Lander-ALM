76d83d00972d01d404c4dd8c8d336ca212646837
paulvercoustre <paulvercoustre@gmail.com>
2017-03-08 13:41:51 +0100

    Changing epochs

--- Lunar_Lander.py
+++ Lunar_Lander.py
@@ -1,6 +1,8 @@
 import gym
 env = gym.make('LunarLanderContinuous-v2')
 env.reset()
-for _ in range(1000):
+for _ in range(100): # testing changes to number of epochs per episode
     env.render()
     env.step(env.action_space.sample())  # take a random action
+
+
