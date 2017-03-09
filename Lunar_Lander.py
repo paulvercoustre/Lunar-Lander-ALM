e75661bbdcc57b43bf75960cdb398e218ea823ed
paulvercoustre <paulvercoustre@gmail.com>
2017-03-09 03:16:53 +0100

    Add Environment Dim

--- Lunar_Lander.py
+++ Lunar_Lander.py
@@ -1,8 +1,25 @@
 import gym
 env = gym.make('LunarLander-v2')
-env.reset()
-for i in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
 
+# get info about the action space
+print(env.action_space)
 
+# ger info about the environment space
+print(env.observation_space)
+print(env.observation_space.high)
+print(env.observation_space.low)
+
+"""
+for i_episode in range(5):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        #print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        #print(observation)
+        print(reward)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+"""
